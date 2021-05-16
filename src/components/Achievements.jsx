de4fd918bab228e2e3ebcd0616d772f79aa41bc9
Rohit <rohit.prasad@plaxonic.com>
2021-05-16 22:05:36 +0530

    :recyle:code updated

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -106,7 +106,7 @@ export default function MiniDrawer() {
   const [data, setData] = useState(null);
   useEffect(() => {
     axios({
-      url : 'http://localhost:8000/content/get-data',
+      url : 'http://rohit-prasad-portfolio.herokuapp.com/content/get-data',
       method : 'get'
   }).then((e) => {
       setData(e.data);
